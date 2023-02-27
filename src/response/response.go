13522afad7eec7cbc747799c032075ac93b50679
Eleven9527 <695923052@qq.com>
2023-02-27 12:01:21 +0800

    update:重构http response结构

--- src/response/response.go
+++ src/response/response.go
@@ -4,19 +4,29 @@ import (
 	errors "customClothing/src/error"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 type response struct {
 	Code int         `json:"code"`
 	Msg  string      `json:"msg"`
+	Time time.Time   `json:"time"`
 	Data interface{} `json:"data"`
 }
 
 func Success(c *gin.Context, err errors.Error, data interface{}) {
 	res := response{
-		Code: err.Code(),
-		Msg:  err.Msg(),
+		Code: 0,
+		Msg:  "success",
+		Time: time.Now(),
 		Data: data,
+	}
+
+	if err != nil {
+		res = response{
+			Code: err.Code(),
+			Msg:  err.Msg(),
+		}
 	}
 
 	//为-1单独添加msg，handler中可以不用加，省事
@@ -32,6 +42,7 @@ func RespError(httpCode int, c *gin.Context, code int, msg string) {
 	res := response{
 		Code: code,
 		Msg:  msg,
+		Time: time.Now(),
 		Data: nil,
 	}
 
